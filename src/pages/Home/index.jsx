c3a6412d9222cb8a3a1f28a18b12540c7abc7355
Fotima <fotimanumojonova777@gmail.com>
2024-02-22 21:16:26 +0500

    Joylashuvlar togirlandi✅

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,17 +7,20 @@ import ProjectSlider from "../portfolio";
 import ProjectVideo from "../Video";
 import Manage from "../../components/Management";
 import Loading from "../../components/loading";
+import Planprojects from "../../components/PlanProjects";
+import ThreeCarousel from "../../components/Carousel";
 
 const Home = () => {
   return (
     <div id="home">
-      <Loading/>
+      <Loading />
       <Navbar />
       <Header />
-      <div className="mt-[700px]">{/* <ThreeCarousel /> */}</div>
+      <div className="mt-[900px]">
+        <ThreeCarousel />
+      </div>
       <Manage />
-      {/* <Planprojects /> */}
-      <div>  
+      <div>
         <section className="portfolio pt-[17px] flex flex-col items-center pb-[100px]">
           <div className="portfolio__heading w-[636px] px-[50px] text-center mb-[60px] max-[991px]:w-[90%] max-[574px]:w-full max-[574px]:px-[12px]">
             <h2 className="text-[30px] text-[#2a2a2a] capitalize leading-[1.46] font-bold mb-[-5px]">
@@ -31,12 +34,12 @@ const Home = () => {
           <div className="slider-content w-full px-[12px] mb-[100px]">
             <ProjectSlider />
           </div>
-          <div>
-            <ProjectVideo />
-            <ContactForm />
-          </div>
         </section>
-
+        <Planprojects />
+        <div className="mt-[330px]">
+          <ProjectVideo />
+        </div>
+        <ContactForm />
         <Footer />
       </div>
     </div>
